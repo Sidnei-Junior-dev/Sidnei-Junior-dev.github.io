ffba8bb0722e74b63db6d72ee1c46f96dbea3b66
Sidnei Junior <135073086+Sidnei-Junior-dev@users.noreply.github.com>
2023-10-19 16:50:23 -0300

    Add files via upload

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -8,3 +8,11 @@ links.forEach(link => {
     section.scrollIntoView({ behavior: "smooth" });
   });
 });
+var btn=document.querySelector(".button1");
+btn.onmousemove=function(e){
+  var x=e.pageX - btn.offsetLeft;
+  var y=e.pageY - btn.offsetTop;
+
+  btn.style.setProperty('--eixoX',x + 'px')
+  btn.style.setProperty('--eixoY',y + 'px')
+}
